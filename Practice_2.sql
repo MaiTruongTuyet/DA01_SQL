bb2775013286fa794109c2a157b38c37122e1386
MaiTruongTuyet <105296674+MaiTruongTuyet@users.noreply.github.com>
2023-11-12 17:17:29 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -1,1 +1,12 @@
+--EX 1
+SELECT DISTINCT CITY FROM STATION
+WHERE ID % 2 = 0
+
+--EX 2
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION
+  
+--EX 4
+SELECT ROUND(((SUM(item_count * order_occurrences) / SUM(order_occurrences)):: NUMERIC),1)
+FROM items_per_order 
 
