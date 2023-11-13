efa846bb7d475832b4c36b168b8c0f1785f8636f
MaiTruongTuyet <105296674+MaiTruongTuyet@users.noreply.github.com>
2023-11-13 22:11:24 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -15,4 +15,16 @@ SELECT DISTINCT candidate_id
 FROM candidates
 WHERE SKILL in('Python','Tableau','PostgreSQL') 
 
+--EX 6
+SELECT USER_ID, DATE(MAX(POST_DATE)) - DATE(MIN(POST_DATE))AS DAYS_BETWEEN
+FROM POSTS
+WHERE POST_DATE BETWEEN '2021-01-01' AND '2022-01-01'
+GROUP BY USER_ID
+HAVING COUNT(POST_ID) >= 2;
+
+-- EX7
+SELECT card_name, MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+
 
