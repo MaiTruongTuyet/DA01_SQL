d62241b178cda1fd040da8d96678432f69d4b778
MaiTruongTuyet <105296674+MaiTruongTuyet@users.noreply.github.com>
2023-11-15 01:17:56 +0700

    Update Practice_3.sql

--- Practice_3.sql
+++ Practice_3.sql
@@ -24,4 +24,31 @@ GROUP BY EXTRACT(MONTH FROM submit_date), PRODUCT_ID
 ORDER BY EXTRACT(MONTH FROM submit_date), PRODUCT_ID
 
 --EX5
+SELECT sender_id, COUNT(message_id) AS message_count
+FROM messages
+WHERE EXTRACT(YEAR FROM sent_date) = 2022
+AND EXTRACT(MONTH FROM sent_date) = 8
+GROUP BY sender_id
+ORDER BY COUNT(message_id) DESC
+LIMIT 2
+
+--EX6
+SELECT  tweet_id
+FROM Tweets
+WHERE LENGTH(CONTENT) > 15
+
+--EX7
+
+--EX8
+SELECT COUNT(ID)
+FROM employees
+WHERE EXTRACT(MONTH FROM joining_date ) BETWEEN 1 AND 7
+AND EXTRACT(MONTH FROM joining_date ) = 2022
+
+--EX9
+SELECT POSITION('a'in first_name )
+FROM worker
+WHERE first_name = 'Amitah'
+
+--EX10
 
