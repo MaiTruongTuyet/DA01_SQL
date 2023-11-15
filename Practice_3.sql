dc949a145d39a2050fb8b5d30b1a3933076aea47
MaiTruongTuyet <105296674+MaiTruongTuyet@users.noreply.github.com>
2023-11-15 11:24:39 +0700

    Update Practice_3.sql

--- Practice_3.sql
+++ Practice_3.sql
@@ -38,6 +38,10 @@ FROM Tweets
 WHERE LENGTH(CONTENT) > 15
 
 --EX7
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
+GROUP BY activity_date;
 
 --EX8
 SELECT COUNT(ID)
